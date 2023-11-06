e377d0e0fea4641d368e3fb5485215de56fa2043
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-11-06 17:56:24 -0300

    Add ServicoOrcamento

---
+++ src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
@@ -0,0 +1,8 @@
+package br.fds.demo.Dominio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public class ServicoOrcamento {
+
+}
