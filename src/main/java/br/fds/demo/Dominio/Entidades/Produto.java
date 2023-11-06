a11d7cbadc6d794ab94907e142f8c9151ef1932c
Vinicius Turani <viniciuscturani@hotmail.com>
2023-11-06 18:17:45 -0300

    Reorganizing the storage for the product entity

--- src/main/java/br/fds/demo/Dominio/Entidades/Produto.java
+++ src/main/java/br/fds/demo/Dominio/Entidades/Produto.java
@@ -2,12 +2,12 @@ package br.fds.demo.Dominio.Entidades;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import br.fds.demo.Dominio.EstoqueProduto;
+// import br.fds.demo.Dominio.EstoqueProduto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+// import jakarta.persistence.OneToOne;
 
 
 @Entity
@@ -16,13 +16,19 @@ public class Produto {
     private long codigo;
     private String descricao;
     private double precoUnitario;
+
+    private int qtdMax;
+    private int qtdMin;
+    private int qtdAtual;
     
-    public Produto(long codigo, String descricao,double precoUnitario, Fornecedor fornecedor, EstoqueProduto estoqueProduto) {
+    public Produto(long codigo, String descricao, double precoUnitario, int qtdMax, int qtdMin,int qtdAtual, Fornecedor fornecedor) {
         this.codigo = codigo;
         this.descricao = descricao;
         this.precoUnitario = precoUnitario;
         this.fornecedor = fornecedor;
-        this.estoqueProduto = estoqueProduto;
+        this.qtdMax = qtdMax;
+        this.qtdMin = qtdMin;
+        this.qtdAtual = qtdAtual;
     }
     
     protected Produto(){}
@@ -32,51 +38,66 @@ public class Produto {
     @JsonBackReference
     private Fornecedor fornecedor;
     
-    public Fornecedor getFornecedor() {
-        return fornecedor;
-    }
-    
     public void setFornecedor(Fornecedor fornecedor) {
         this.fornecedor = fornecedor;
     }
 
-    @OneToOne(mappedBy = "produto")
-    @JsonBackReference
-    private EstoqueProduto estoqueProduto;
-    public EstoqueProduto getEstoqueProduto() {
-        return estoqueProduto;
+    public Fornecedor getFornecedor() {
+        return fornecedor;
     }
-
-    public void setEstoqueProduto(EstoqueProduto estoqueProduto) {
-        this.estoqueProduto = estoqueProduto;
+    
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
     }
-
 
     public long getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(long codigo) {
-        this.codigo = codigo;
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
-
+    
     public String getDescricao() {
         return descricao;
     }
-
+    
     public void setPrecoUnitario(long precoUnitario){
         this.precoUnitario = precoUnitario;
     }
-
+    
     public double getPrecoUnitario() {
         return precoUnitario;
+    }
+    
+    public void setQtdMax(int qtdMax){
+        this.qtdMax = qtdMax;
+    }
+    
+    public int getQtdMax() {
+        return qtdMax;
+    }
+    
+    public void setQtdMin(int qtdMin){
+        this.qtdMin = qtdMin;
+    }
+    
+    public int getQtdMin() {
+        return qtdMin;
+    }
+
+    public void setQtdAtual(int qtdAtual){
+        this.qtdAtual = qtdAtual;
+    }
+    
+    public int getQtdAtual() {
+        return qtdAtual;
     }
 
     @Override
     public String toString() {
-        return "Produto [codigo=" + codigo 
-                + ", descricao=" + descricao + ", precoUnitario=" + precoUnitario 
-                + fornecedor.toString()
+        return "Produto [codigo=" + codigo + ", descricao=" + descricao + ", precoUnitario=" + precoUnitario
+                + ", qtdMax=" + qtdMax + ", qtdMin=" + qtdMin + ", qtdAtual=" + qtdAtual + ", fornecedor=" + fornecedor
                 + "]";
     }
 }
