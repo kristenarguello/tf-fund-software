398820f7238ca1981bde2e4ffa0bb6a6d169d3a1
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-11-06 18:09:17 -0300

    Add Autowired and Service

--- src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
+++ src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
@@ -3,6 +3,11 @@ package br.fds.demo.Dominio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class ServicoOrcamento {
+
+    @Autowired
+    public ServicoOrcamento() {
+    }
 
 }
