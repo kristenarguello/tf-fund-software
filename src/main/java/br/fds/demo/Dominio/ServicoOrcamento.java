5f97cf9167e56bf5708e380a7fdeaa2d352c3590
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-11-08 14:45:29 -0300

    Add constructor to ServicoOrcamento

--- src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
+++ src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
@@ -1,13 +1,18 @@
 package br.fds.demo.Dominio;
 
+import br.fds.demo.Dominio.Entidades.Cliente;
+import br.fds.demo.Dominio.Entidades.Orcamento;
+import br.fds.demo.Dominio.Entidades.Pedido;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServicoOrcamento {
+    private Orcamento orcamento;
 
     @Autowired
-    public ServicoOrcamento() {
+    public ServicoOrcamento(Pedido pedido, float percentualImposto, float percentualDesconto) {
+        this.orcamento = new Orcamento(pedido, percentualImposto, percentualDesconto);
     }
 
 }
