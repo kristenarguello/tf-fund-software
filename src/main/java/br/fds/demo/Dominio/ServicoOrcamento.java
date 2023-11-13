2ec7ba49f09019f224a413aab2e7ec94dba68ab2
RamiroNB <ramiro.nilson.barros@gmail.com>
2023-11-13 18:19:07 -0300

    Add geraOrcamento

--- src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
+++ src/main/java/br/fds/demo/Dominio/ServicoOrcamento.java
@@ -1,8 +1,9 @@
 package br.fds.demo.Dominio;
 
+import br.fds.demo.Dominio.Entidades.Pedido;
 import br.fds.demo.Dominio.Entidades.ItemPedido;
 import br.fds.demo.Dominio.Entidades.Orcamento;
-import jakarta.transaction.Transactional;
+import br.fds.demo.Dominio.OpcoesDesconto.FabricaDeDescontos;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -19,37 +20,47 @@ public class ServicoOrcamento {
     @Autowired
     private IRepOrcamentos repOrcamentos;
 
-    @Transactional
+    @Autowired
+    private FabricaDeDescontos fabricaDeDescontos;
+
     public void efetivarOrcamento(long id) throws ResponseStatusException {
         // obter a entidade do orcamento
         Orcamento orcamento = repOrcamentos.buscarPorId(id);
-        if (orcamento == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "O orçamento não pôde ser encontrado");
+        if (orcamento == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "O orçamento não pôde ser encontrado");
 
         // validar as datas dos orcamentos
         long diasDesdeEmissaoOrcamento = Duration.between(LocalDateTime.now(), orcamento.getData()).toDays();
         int diasValidadeOrcamento = calcularDiasValidade(orcamento);
-        if (diasDesdeEmissaoOrcamento > diasValidadeOrcamento) 
+        if (diasDesdeEmissaoOrcamento > diasValidadeOrcamento)
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O orçamento está vencido");
-        
+
         // validar se ha produtos o suficiente
         for (ItemPedido ip : orcamento.getPedido().getItens()) {
             int qtdAtualProduto = ip.getProduto().getQtdAtual();
             if (qtdAtualProduto < ip.getQuantidade()) {
-                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, 
-                                                  String.format("No momento, o produto de descrição '%s' não possui unidades suficientes", 
-                                                                ip.getProduto().getDescricao()));
+                throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY,
+                        String.format("No momento, o produto de descrição '%s' não possui unidades suficientes",
+                                ip.getProduto().getDescricao()));
             }
-            ip.getProduto().setQtdAtual(qtdAtualProduto-ip.getQuantidade());
+            ip.getProduto().setQtdAtual(qtdAtualProduto - ip.getQuantidade());
         }
 
-        // tudo ocorreu corretamente. marcar orcamento como aprovado e salvar no banco de dados
+        // tudo ocorreu corretamente. marcar orcamento como aprovado e salvar no banco
+        // de dados
         orcamento.aprovarOrcamento();
         repOrcamentos.salvar(orcamento);
     }
 
     private int calcularDiasValidade(Orcamento orcamento) {
         int mesEmissao = orcamento.getData().getMonthValue();
-        return mesEmissao == 12 || mesEmissao == 1 || mesEmissao == 2 || mesEmissao == 7?
-            DIAS_VALIDADE_BAIXA_PROCURA : DIAS_VALIDADE_ALTA_PROCURA;
+        return mesEmissao == 12 || mesEmissao == 1 || mesEmissao == 2 || mesEmissao == 7 ? DIAS_VALIDADE_BAIXA_PROCURA
+                : DIAS_VALIDADE_ALTA_PROCURA;
+    }
+
+    public void gerarOrcamento(Pedido Pedido) {
+        float percentualDesconto = fabricaDeDescontos.getDesconto(Pedido);
+        Orcamento orcamento = new Orcamento(Pedido, 0.10f, percentualDesconto);
+        repOrcamentos.salvar(orcamento);
     }
 }
