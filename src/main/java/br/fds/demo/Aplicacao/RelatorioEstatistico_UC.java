9804a8a1e59f8ab78d65df300e5a2dbe9c6dd373
Kristen <kristenarguello20@gmail.com>
2023-11-15 22:28:40 -0300

    Use Rep instead of Service for statistics

--- src/main/java/br/fds/demo/Aplicacao/RelatorioEstatistico_UC.java
+++ src/main/java/br/fds/demo/Aplicacao/RelatorioEstatistico_UC.java
@@ -3,13 +3,20 @@ package br.fds.demo.Aplicacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import br.fds.demo.Dominio.ServicoEstatistico;
+import br.fds.demo.Persistencia.RepClientesJPA;
+import br.fds.demo.Persistencia.RepOrcamentosJPA;
+import br.fds.demo.Persistencia.RepProdutosJPA;
 
 @Component
 public class RelatorioEstatistico_UC {
     @Autowired
-    private ServicoEstatistico servicoEstatistico;
+    private RepProdutosJPA produtosRep;
 
-    
+    @Autowired
+    private RepClientesJPA clientesRep;
+
+    @Autowired
+    private RepOrcamentosJPA orcamentosRep;
+
 }
 
